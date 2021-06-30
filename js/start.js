34add90b39e83a42adb26c02cb6ef9947037601b
dayoung lee <75241425+leedy-s2@users.noreply.github.com>
2021-06-30 21:54:03 +0900

    Update start.js

--- js/start.js
+++ js/start.js
@@ -6,10 +6,9 @@ const endPoint = 13;
 const choice1 = [];
 const choice2 = [];
 var point = 0;
-var level = 0;
 
+// 획득포인트(value)에 따라 레벨(key)을 가져와 반환한다.
 function getKeyByValue(point) {
-  console.log(point);
   for(var key in fshipLevel){
     if(fshipLevel[key].includes(point)){
       return key;
@@ -18,6 +17,7 @@ function getKeyByValue(point) {
   return 'no answer';
 }
 
+// choice1과 choice2의 값 비교 후 포인트 부여
 function calResult(){
   for(let i = 0; i<choice1.length; i++){
     if(choice1[i]==choice2[i]){
@@ -28,7 +28,7 @@ function calResult(){
   return point;
 }
 
-
+// 출제자가 문제를 다 풀 경우 정답을 배열에 넣고 메인 화면으로 복귀
 function goMain(){
   qna.style.WebkitAnimation="fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
@@ -42,19 +42,20 @@ function goMain(){
   },450)
   }
 
+
+  // 출제자와 참여자의 클릭 인덱스를 비교하여 점수 및 레벨 부여
   function setResult(){
     point = calResult();
     var level = getKeyByValue(point);
+
     const friendName = document.querySelector('.friendName');
     var self = document.querySelector('.txt_name').value;
     var friend = document.querySelector('.txt_friend').value;
     friendName.innerHTML = self +'와 '+friend+'의';
 
-    var friendScore = Math.round(point*100/endPoint);
+    var friendScore = Math.round(point/endPoint*100);
     const score = document.querySelector('.score');
     score.innerHTML = '우정모의고사 점수는 '+friendScore+'점';
-
-    console.log(level);
 
     const resultName = document.querySelector('.resultName');
     console.log(infoList[level].name);
@@ -71,11 +72,10 @@ function goMain(){
 
     const resultDesc = document.querySelector('.resultDesc');
     resultDesc.innerHTML = infoList[level].desc;
-
-    point = 0;
-    level = 0;
   }
 
+
+  // 결과 페이지가 나타나는 함수
   function goResult(){
     qna.style.WebkitAnimation="fadeOut 1s";
     qna.style.animation = "fadeOut 1s";
@@ -93,6 +93,8 @@ function goMain(){
       setResult();
     }
 
+
+    // 각 answer를 fadeIn해주고 click이 발생할 경우 해당 index를 배열에 담고 다음 문제로
     function addAnswer(answerText, qIdx, idx, diff){
       var a = document.querySelector('.ansBox');
       var answer = document.createElement('button');
@@ -131,6 +133,8 @@ function goMain(){
       }, false);
     }
 
+
+    // qIdx번째 질문을 제시하고, 상태바를 제작한 뒤 addAnswer함수로 이동
     function goNext(qIdx, diff){
       if(qIdx === endPoint){
         if(diff === 1){
@@ -140,21 +144,21 @@ function goMain(){
           goResult();
           return;
         }
-
       }
+      var status = document.querySelector('.statusBar');
+      status.style.width = (100/endPoint) * (qIdx+1) +'%';
+
       var q = document.querySelector('.qBox');
       q.innerHTML = qnaList[qIdx].q;
       for(let i in qnaList[qIdx].a){
         addAnswer(qnaList[qIdx].a[i], qIdx, i, diff);
       }
-      var status = document.querySelector('.statusBar');
-      status.style.width = (100/endPoint) * (qIdx+1) +'%';
+
     }
 
 
-
+    // 메인페이지에서 qna 페이지로 처음 이동
     function begin(diff){
-
       main.style.WebkitAnimation="fadeOut 1s";
       main.style.animation = "fadeOut 1s";
       setTimeout(() => {
